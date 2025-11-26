8e2b54314686a0d6ea712b83879a92f8437d65a4
Harry7986 <emarh.harry.code@gmail.com>
2025-11-26 06:48:41 +0100

    Loader

---
+++ lib/markdown/loader.ts
@@ -0,0 +1,25 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import { remark } from "remark";
+import html from "remark-html";
+
+const postsDir = path.join(process.cwd(), "src/content/posts");
+
+export async function getPost(slug: string) {
+  const file = fs.readFileSync(path.join(postsDir, `${slug}.md`), "utf-8");
+  const { data, content } = matter(file);
+
+  const htmlContent = await remark().use(html).process(content);
+
+  return {
+    ...data,
+    content: htmlContent.toString(),
+  };
+}
+
+export function getAllPosts() {
+  return fs.readdirSync(postsDir).map((filename) => ({
+    slug: filename.replace(".md", ""),
+  }));
+}
