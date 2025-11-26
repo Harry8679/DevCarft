d33fa1c7aadff464832470f8c6e66bf4687c1689
Harry7986 <emarh.harry.code@gmail.com>
2025-11-26 06:23:49 +0100

    Contact 2

---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+import { ContactForm } from "@/components/forms/ContactForm";
+
+export default function Contact() {
+  return (
+    <section>
+      <h2 className="text-4xl font-bold mb-6">Contact</h2>
+      <ContactForm />
+    </section>
+  );
+}
